3221428869cb0dc3f8e0f5d116027118fdaa4535
NataliaHopper <nhopper@usc.edu>
2025-04-30 19:18:09 -0700

    Create DisclaimerCode.m

---
+++ DisclaimerCode.m
@@ -0,0 +1,4 @@
+%% Disclaimer
+display('Please only input rectangles, squares, or right triangles that have the base at the bottom of the triangle.')
+display('Additionally, input the vertices in the order that you would connect them if you were playing connect the dots!')
+display('Thank you!')
